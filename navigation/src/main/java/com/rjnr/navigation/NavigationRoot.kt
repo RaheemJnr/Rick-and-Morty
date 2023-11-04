90a1706a6213a614324c15aba4e7564b12f86579
Raheem Jnr <mumedian6@gmail.com>
2023-11-04 19:47:32 +0100

    added navigation module

--- navigation/src/main/java/com/rjnr/navigation/NavigationRoot.kt
+++ navigation/src/main/java/com/rjnr/navigation/NavigationRoot.kt
@@ -1,2 +1,30 @@
 package com.rjnr.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.compositionLocalOf
+
+
+private val localNavigation = compositionLocalOf<Navigation> { error("No Local Navigation") }
+
+@Composable
+fun NavigationRoot(
+    navigation: Navigation,
+    navGraph: @Composable (screen: Screen?) -> Unit
+) {
+    CompositionLocalProvider(
+        localNavigation provides navigation,
+    ) {
+        val viewModelStore = LocalViewModelStoreOwner.current
+        DisposableEffect(navigation.currentScreen) {
+            onDispose {
+                // Destroy viewModels only for non-legacy screens
+                if (navigation.lastScreen?.isLegacy == false) {
+                    viewModelStore?.viewModelStore?.clear()
+                }
+            }
+        }
+        navGraph(navigation.currentScreen)
+    }
+}
