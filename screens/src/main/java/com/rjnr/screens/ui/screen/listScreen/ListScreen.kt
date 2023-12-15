a3f8e8d4683e61c4fdcf54cd9acafcea42d71af3
Raheem Jnr <mumedian6@gmail.com>
2023-12-15 13:03:02 +0100

    type: reformatting
    body: added new network and domain models

--- screens/src/main/java/com/rjnr/screens/ui/screen/listScreen/ListScreen.kt
+++ screens/src/main/java/com/rjnr/screens/ui/screen/listScreen/ListScreen.kt
@@ -1,15 +1,15 @@
 package com.rjnr.screens.ui.screen.listScreen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -23,26 +23,30 @@ import com.rjnr.screens.ui.screen.composeExt.onScreenStart
 @Composable
 fun ListScreen(screen: ListScreen) {
     val viewModel: ListViewModel = viewModel()
-    val uiState by viewModel.uiState.collectAsState()
     val page = viewModel.page.intValue
+    val loading = viewModel.loading.value
+    val character = viewModel.character.value
+    Log.i("item size", "position:${character.size}")
     val nav = navigation()
     onScreenStart {
         viewModel.start()
     }
     List(
         navigation = nav,
-        state = uiState,
         viewModel = ListViewModel(),
         page = page,
+        loading = loading,
+        character = character,
     )
 }
 
 @Composable
 fun List(
     navigation: Navigation,
-    state: UIDataState,
     viewModel: ListViewModel,
     page: Int,
+    loading: Boolean,
+    character: List<Character>,
 ) {
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -50,22 +54,29 @@ fun List(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         LazyColumn() {
-            if (state.loading && state.character.isEmpty()) {
+            if (loading && character.isEmpty()) {
                 item {
-                    Text(text = "Loading")
+                    Loading()
                 }
             } else {
-                itemsIndexed(state.character) { index, item ->
+                itemsIndexed(character) { index, item ->
                     viewModel.onChangeItemScrollPosition(index)
+                    Log.i("emitting data", "index:$index")
 
-                    ListView(uiState = item)
-                    if ((index + 1) >= (page * PAGE_SIZE) && !state.loading) {
+                    if ((index + 1) >= (page * PAGE_SIZE) && !loading) {
                         viewModel.nextPage()
+                        Log.i("item size", "position:${character.size}")
                     }
+                    ListView(uiState = item)
                 }
             }
         }
     }
+}
+
+@Composable
+fun Loading() {
+    CircularProgressIndicator()
 }
 
 @Composable
--- screens/src/main/java/com/rjnr/screens/ui/screen/listScreen/Viewmodel.kt
+++ screens/src/main/java/com/rjnr/screens/ui/screen/listScreen/Viewmodel.kt
@@ -1,7 +1,9 @@
 package com.rjnr.screens.ui.screen.listScreen
 
 import android.util.Log
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rjnr.navigation.Navigation
@@ -9,19 +11,14 @@ import com.rjnr.networking.repo.Repo
 import com.rjnr.networking.repo.RepoImpl
 import com.rjnr.screens.ui.domain.Character
 import com.rjnr.screens.ui.domain.mapper.toEntity
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 const val PAGE_SIZE = 20
 
-data class UIDataState(
-    val loading: Boolean = true,
-    val character: List<Character> = ArrayList(),
-)
+// data class UIDataState(
+//    val loading: Boolean = true,
+//    val character: List<Character> = ArrayList(),
+// )
 
 class ListViewModel(
     private val nav: Navigation = Navigation(),
@@ -29,9 +26,11 @@ class ListViewModel(
 ) : ViewModel() {
 
     val page = mutableIntStateOf(1)
+    val character: MutableState<List<Character>> = mutableStateOf(ArrayList())
+    val loading = mutableStateOf(true)
     private var itemListScrollPosition = 0
-    private val _uiState = MutableStateFlow(UIDataState())
-    val uiState: StateFlow<UIDataState> = _uiState.asStateFlow()
+//    private val _uiState = MutableStateFlow(UIDataState())
+//    val uiState: StateFlow<UIDataState> = _uiState.asStateFlow()
 
     init {
         start()
@@ -39,18 +38,14 @@ class ListViewModel(
 
     fun start() {
         viewModelScope.launch {
+            loading.value = true
             try {
-                _uiState.update { state ->
-                    state.copy(loading = true)
-                }
-                val result = repo.getAllCharacters(page = page.intValue)
-                _uiState.update { characterResponse ->
-                    characterResponse.copy(character = result.toEntity().results, loading = false)
-                }
+                val result = repo.getAllCharacters(page = 1)
+                Log.i("first page number", "pageNumber:${page.intValue}")
+                character.value = result.toEntity().results
+                Log.i("emitting data", ":${result.toEntity().info}")
+                loading.value = false
             } catch (e: Exception) {
-                _uiState.update { state ->
-                    state.copy(loading = false)
-                }
                 Log.e("emitting failure data", e.toString())
             }
         }
@@ -62,30 +57,32 @@ class ListViewModel(
         viewModelScope.launch {
             // check if scroll position (20 at the start) + 1  is greater than
             // page * Page size(20 at the start) if true fetch new data
-            if ((itemListScrollPosition + 1) >= (page.intValue * PAGE_SIZE)) {
+            if (itemListScrollPosition + 1 >= (page.intValue * PAGE_SIZE)) {
+                Log.i("scroll position1", "position:$itemListScrollPosition")
                 // add loading to true
-                _uiState.update { state ->
-                    state.copy(loading = true)
-                }
+                loading.value = true
                 incrementPage()
-                // to show loading
-                delay(1000)
+                Log.i("emitting data", "pageNumber:${page.intValue}")
 
                 if (page.intValue > 1) {
-                    val result = repo.getAllCharacters(page = page.intValue)
-                    appendNewItems(result.toEntity().results)
+                    try {
+                        val result = repo.getAllCharacters(page = page.intValue)
+                        Log.i("emitting data", "${page.intValue}")
+                        appendNewItems(result.toEntity().results)
+                        Log.i("emitting data", "${result.toEntity()}")
+                    } catch (e: Exception) {
+                        Log.e("emitting failure data", e.toString())
+                    }
                 }
-                _uiState.update { state ->
-                    state.copy(loading = false)
-                }
+                loading.value = false
             }
         }
     }
 
     private fun appendNewItems(items: List<Character>) {
-        val currentList = ArrayList(this._uiState.value.character)
+        val currentList = ArrayList(character.value)
         currentList.addAll(items)
-        this._uiState.update { it.copy(character = currentList) }
+        character.value = currentList
     }
 
     private fun incrementPage() {
@@ -94,5 +91,6 @@ class ListViewModel(
 
     fun onChangeItemScrollPosition(position: Int) {
         itemListScrollPosition = position
+        Log.i("scroll position", "position:$position")
     }
 }
