4582a2110782f41ad6362d50e72a31f838b37f67
Raheem Jnr <mumedian6@gmail.com>
2023-12-24 16:34:01 +0100

    type: reformatting
    body:fixed the pagination error by passing the right instance of the viewmodel instead of instantiating another one

--- app/src/main/java/com/rjnr/rickandmorty/NavGraph.kt
+++ app/src/main/java/com/rjnr/rickandmorty/NavGraph.kt
@@ -5,21 +5,14 @@ import com.rjnr.navigation.DetailScreen
 import com.rjnr.navigation.ListScreen
 import com.rjnr.navigation.OnboardingScreen
 import com.rjnr.navigation.Screen
-<<<<<<< HEAD
-import com.rjnr.screens.ui.screen.detailScreen.DetailScreen
+import com.rjnr.screens.ui.screen.detail.DetailScreen
 import com.rjnr.screens.ui.screen.list.ListScreen
 import com.rjnr.screens.ui.screen.onboarding.OnboardingScreen
-=======
-import com.rjnr.screens.ui.screen.listScreen.ListScreen
->>>>>>> master
-
 
 @Composable
 fun NavGraph(screen: Screen?) {
-
     when (screen) {
         null -> {
-
         }
         is OnboardingScreen -> {
             OnboardingScreen(screen = screen)
@@ -30,12 +23,7 @@ fun NavGraph(screen: Screen?) {
         }
 
         is DetailScreen -> {
-
             DetailScreen(screen = screen)
-
         }
-
-
     }
-
 }
