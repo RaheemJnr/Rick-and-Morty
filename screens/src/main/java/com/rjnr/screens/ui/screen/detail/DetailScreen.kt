943aeba6ec632d5c25df21a97949dbd7cb8066fd
Raheem Jnr <mumedian6@gmail.com>
2024-01-16 19:04:00 +0100

    type: reformatting
    body:detail screen unexpected error

--- screens/src/main/java/com/rjnr/screens/ui/screen/detail/DetailScreen.kt
+++ screens/src/main/java/com/rjnr/screens/ui/screen/detail/DetailScreen.kt
@@ -1,57 +1,79 @@
 package com.rjnr.screens.ui.screen.detail
 
+import android.util.Log
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.rjnr.navigation.DetailScreen
 import com.rjnr.navigation.navigation
+import com.rjnr.screens.ui.domain.Character
 import com.rjnr.screens.ui.screen.composeExt.onScreenStart
 
+@Suppress("ktlint:standard:function-naming")
 @Composable
 fun DetailScreen(
     modifier: Modifier = Modifier,
     screen: DetailScreen,
 ) {
     val viewModel = DetailsViewModel()
+    val details by viewModel.uiState.collectAsState()
     val navigation = navigation()
     onScreenStart {
         viewModel.getCharacterDetails(screen.id)
     }
-    val details by viewModel.singleCharacter
+
+    DetailUI(modifier, details)
+
+    BackHandler {
+        navigation.onBackPressed()
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+private fun DetailUI(
+    modifier: Modifier,
+    details: Character,
+) {
     Column(
         modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
             text = details.name,
         )
-        Text(
-            text = "${details.id}",
-        )
-        Text(
-            text = details.gender,
-        )
-        Text(
-            text = details.species,
-        )
-        Button(
-            onClick = {
-                viewModel.getCharacterDetails(screen.id)
-            },
-        ) {
-            Text(text = "Hi, this is the details screen")
-        }
-    }
+        Log.i("Details fetched list", "$details")
 
-    BackHandler {
-        navigation.onBackPressed()
     }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPrev() {
+    DetailUI(
+        modifier = Modifier,
+        details =
+            Character(
+                id = 1,
+                name = "Jnr",
+                status = "Alive",
+                species = "human",
+                type = "",
+                gender = "male",
+                origin = null,
+                location = null,
+                image = "",
+                episode = listOf(),
+                url = "",
+                created = "",
+            ),
+    )
 }
--- screens/src/main/java/com/rjnr/screens/ui/screen/detail/Viewmodel.kt
+++ screens/src/main/java/com/rjnr/screens/ui/screen/detail/Viewmodel.kt
@@ -4,23 +4,24 @@ import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.rjnr.navigation.Navigation
 import com.rjnr.networking.model.CharacterDTO
 import com.rjnr.networking.repo.Repo
 import com.rjnr.networking.repo.RepoImpl
+import com.rjnr.screens.ui.domain.Character
 import com.rjnr.screens.ui.domain.mapper.toEntity
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class DetailsViewModel(
-    private val nav: Navigation = Navigation(),
     private val repo: Repo = RepoImpl(),
 ) : ViewModel() {
-    val singleCharacter = mutableStateOf(CharacterDTO().toEntity())
-    val loading = mutableStateOf(true)
+    private val _uiState = MutableStateFlow(CharacterDTO().toEntity())
+    val uiState: StateFlow<Character> = _uiState.asStateFlow()
 
-    init {
-        start()
-    }
+    // val singleCharacter: MutableState<Character> = mutableStateOf(CharacterDTO().toEntity())
+    val loading = mutableStateOf(true)
 
     fun start() {
     }
@@ -30,8 +31,8 @@ class DetailsViewModel(
             loading.value = true
             try {
                 val result = repo.getSingleCharacter(id = id)
-                singleCharacter.value = result.toEntity()
-                Log.i("emitting failure data", result.toString())
+                // singleCharacter.value = result.toEntity()
+                _uiState.value = result.toEntity()
                 loading.value = false
             } catch (e: Exception) {
                 Log.e("emitting failure data", e.toString())
